45ed8754828d14c51cce8decb86d9834f70a8e7b
Kensuke Shibata <kensukeshibata@gmail.com>
2019-08-22 12:44:43 +1000

    added CarList Component

---
+++ client/src/Components/CarList.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+class CarList extends Component {
+  render() {
+    return (
+      <div>
+        <ul>
+          <li>Car Name </li>
+        </ul>
+
+      </div>
+    );
+  }
+}
+
+export default CarList;
+
+
+
