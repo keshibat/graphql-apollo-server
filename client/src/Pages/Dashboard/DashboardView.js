77ca7066f5c7308c13442e177b80c75b281a19e8
Kensuke Shibata <kensukeshibata@gmail.com>
2019-08-22 12:46:53 +1000

    fixed commit

--- client/src/Pages/Dashboard/DashboardView.js
+++ client/src/Pages/Dashboard/DashboardView.js
@@ -4,12 +4,15 @@ import LastFillup from "../../Components/Card/LastFillupCard"
 import GoogleMap from "../../Components/GoogleMap/GoogleMap"
 import AntdTable from "../../Components/AntdTable/AntdTable"
 
+import CarList from "../../Components/CarList"
+
 
 class DashboardView extends Component {
   render() {
     return (
       <div>
         <h1>Dashboard</h1>
+        <CarList />
         <LastFillup />
         <GoogleMap />
         <AntdTable />
--- client/src/index.js
+++ client/src/index.js
@@ -2,4 +2,7 @@ import React from "react"
 import ReactDOM from "react-dom"
 import App from "./App"
 
+//For the time being style
+import './index.css';
+
 ReactDOM.render(<App />, document.getElementById("root"))
