88c214f05c9e73356a54c8378beb990038acdf66
Kensuke Shibata <kensukeshibata@gmail.com>
2019-08-22 16:50:51 +1000

    setup the state

--- client/src/Components/AddCar.js
+++ client/src/Components/AddCar.js
@@ -1,11 +1,36 @@
 import React, { Component } from "react";
 import { graphql } from "react-apollo";
-
 import { getOwnerQuery } from "../queris/queris";
-
+import { bind } from "@wry/context";
 
 
 class AddCar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      carName: '',
+      latfillup: 0,
+      latfilluptime: '',
+      lastfilluplocation: "",
+      fuelleft: 0,
+      traveldsince: 0,
+      diagnostic: "",
+      diagnosticdetail: "",
+      businessratio: 0,
+      businesstotal: 0,
+      averagespeed: 0,
+      traveldistancetotal: 0,
+      traveldistancethisyear: 0,
+      timeincar: 0,
+      emissions: 0,
+      fueleconomy: 0,
+      parking: "",
+      startlocation: "",
+      endlocation: "",
+      ownerId: ''
+    };
+  }
+
   displayAuthor(){
     let data = this.props.data;
     if(data.loading){
@@ -17,20 +42,97 @@ class AddCar extends Component {
     }
   }
 
+  submitForm(e){
+    e.preventDefault()
+    console.log(this.state);
+  }
+
 
   render() {
     return (
-      <form id="add-car">
+      <form id="add-car" onSubmit={this.submitForm.bind(this)}>
         <div className="field">
-          <label>Car name:</label>
-          <input type="text" />
+          <label>carName:</label>
+          <input type="text" onChange={(e) => this.setState({ carName: e.target.value})}/>
         </div>
         <div className="field">
-          <label>Fuel Left:</label>
-          <input type="text" />
+          <label>latfillup:</label>
+          <input type="number" onChange={(e) => this.setState({ latfillup: e.target.value })}/>
         </div>
         <div className="field">
-          <label>Average:</label>
+          <label>latfilluptime:</label>
+          <input type="text" onChange={(e) => this.setState({ latfilluptime: e.target.value })}/>
+        </div>
+        <div className="field">
+          <label>lastfilluplocation:</label>
+          <input type="text" onChange={(e) => this.setState({ lastfilluplocation: e.target.value })} />
+        </div>
+        <div className="field">
+          <label>fuelleft:</label>
+          <input type="number" onChange={(e) => this.setState({ fuelleft: e.target.value })}/>
+        </div>
+        <div className="field">
+          <label>traveldsince:</label>
+          <input type="number" onChange={(e) => this.setState({ traveldsince: e.target.value })}/>
+        </div>
+        <div className="field">
+          <label>diagnostic:</label>
+          <input type="text" onChange={(e) => this.setState({ diagnostic: e.target.value })}/>
+        </div>
+        <div className="field">
+          <label>diagnosticdetail:</label>
+          <input type="text" onChange={(e) => this.setState({ diagnosticdetail: e.target.value })}/>
+        </div>
+        <div className="field">
+          <label>businessratio:</label>
+          <input type="number" onChange={(e) => this.setState({ businessratio: e.target.value })}/>
+        </div>
+        <div className="field">
+          <label>businesstotal:</label>
+          <input type="number" onChange={(e) => this.setState({ businesstotal: e.target.value })}/>
+        </div>
+        <div className="field">
+          <label>averagespeed:</label>
+          <input type="number" onChange={(e) => this.setState({ averagespeed: e.target.value })}/>
+        </div>
+        <div className="field">
+          <label>traveldistancetotal:</label>
+          <input type="number" onChange={(e) => this.setState({ traveldistancetotal: e.target.value })}/>
+        </div>
+        <div className="field">
+          <label>traveldistancethisyear:</label>
+          <input type="number" onChange={(e) => this.setState({ traveldistancethisyear: e.target.value })}/>
+        </div>
+        <div className="field">
+          <label>timeincar:</label>
+          <input type="number" onChange={(e) => this.setState({ timeincar: e.target.value })}/>
+        </div>
+        <div className="field">
+          <label>emissions:</label>
+          <input type="number" onChange={(e) => this.setState({ emissions: e.target.value })}/>
+        </div>
+        <div className="field">
+          <label>fueleconomy:</label>
+          <input type="number" onChange={(e) => this.setState({ fueleconomy: e.target.value })}/>
+        </div>
+        <div className="field">
+          <label>parking:</label>
+          <input type="text" onChange={(e) => this.setState({ parking: e.target.value })}/>
+        </div>
+        <div className="field">
+          <label>timetraveld:</label>
+          <input type="text" onChange={(e) => this.setState({ startlocation: e.target.value })}/>
+        </div>
+        <div className="field">
+          <label>startlocation:</label>
+          <input type="text" onChange={(e) => this.setState({ endlocation: e.target.value })}/>
+        </div>
+        <div className="field">
+          <label>endlocation:</label>
+          <input type="text" onChange={(e) => this.setState({ ownerId: e.target.value })}/>
+        </div>
+        <div className="field">
+          <label>Owner:</label>
           <select>
             <option>Select Owner</option>
             {this.displayAuthor()}
