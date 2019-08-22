4ab125e94df46d73763a5588a8c618a9419b8883
Kensuke Shibata <kensukeshibata@gmail.com>
2019-08-22 13:09:32 +1000

    setting up apollo server

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,32 @@
 import React from "react"
 import { BrowserRouter, Route } from "react-router-dom";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
 import DashboardView from "./Pages/Dashboard/DashboardView";
 import LoginView from "./Pages/Login/LoginView";
+
+
+//Apollo client setup
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql"
+})
+
 
 
 class App extends React.Component {
 
   render() {
     return (
-      <>
-        <BrowserRouter>
-          <div>
-            <Route exact path="/" component={DashboardView} />
-            <Route exact path="/login" component={LoginView} />
-          </div>
-        </BrowserRouter>
-      </>
+      <ApolloProvider client={client}>
+        <>
+          <BrowserRouter>
+            <div>
+              <Route exact path="/" component={DashboardView} />
+              <Route exact path="/login" component={LoginView} />
+            </div>
+          </BrowserRouter>
+        </>
+      </ApolloProvider>
     )
   }
 }
