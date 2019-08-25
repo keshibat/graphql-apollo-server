95d30468ea14751ce7ddcc10d7a8aa04b5cfff54
Kensuke Shibata <kensukeshibata@gmail.com>
2019-08-25 19:59:45 +1000

    added marker maping dummy json data

--- client/src/Components/GoogleMap/GoogleMap.js
+++ client/src/Components/GoogleMap/GoogleMap.js
@@ -1,29 +1,30 @@
 import React, { Component } from "react";
 import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
-
-
+import * as parkData from "../../data/skateboard-parks.json";
 
 
 class SimpleMap extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      stores: [{ lat: 47.49855629475769, lng: -122.14184416996333 },
-      { latitude: 47.359423, longitude: -122.021071 },
-      { latitude: 47.2052192687988, longitude: -121.988426208496 },
-      { latitude: 47.6307081, longitude: -122.1434325 },
-      { latitude: 47.3084488, longitude: -122.2140121 },
-      { latitude: 47.5524695, longitude: -122.0425407 }]
-    }
+    // this.state = {
+    //   stores: [{ lat: 47.49855629475769, lng: -122.14184416996333 },
+    //   { latitude: 47.359423, longitude: -122.021071 },
+    //   { latitude: 47.2052192687988, longitude: -121.988426208496 },
+    //   { latitude: 47.6307081, longitude: -122.1434325 },
+    //   { latitude: 47.3084488, longitude: -122.2140121 },
+    //   { latitude: 47.5524695, longitude: -122.0425407 }]
+    // }
   }
 
   displayMarkers = () => {
-    return this.state.stores.map((store, index) => {
-      return <Marker key={index} id={index} position={{
-        lat: store.latitude,
-        lng: store.longitude
-      }}
+      return parkData.features.map((park) => {
+      return <Marker
+              key={park.properties.PARK_ID}
+              position={{
+                lat: park.geometry.coordinates[1],
+                lng: park.geometry.coordinates[0]
+              }}
         onClick={() => console.log("You clicked me!")} />
     })
   }
@@ -34,7 +35,7 @@ class SimpleMap extends Component {
       <Map
         google={this.props.google}
         zoom={11}
-        initialCenter={{ lat: 47.444, lng: -122.176 }}
+          initialCenter={{ lat: 45.4211, lng: -75.6903 }}
       >
         {this.displayMarkers()}
       </Map>
